4c6596e62e4474d7d038eff53b67803a37512463
Robert <soulliberty@gmail.com>
2019-01-15 19:31:09 -0600

    adding periods page stub

---
+++ src/pages/periods.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/layout.js';
+import '../components/layout.css'
+
+
+class PeriodsPage extends React.Component {
+
+  render() {
+    const { data } = this.props
+    return (
+      <Layout>
+        <div className="container">
+          <h1>Timeline of the Bible</h1>
+        </div>
+        <div className="footer"></div>
+      </Layout>)
+  }
+}
+
+export default PeriodsPage
+
+export const query = graphql
+`
+{
+  allAirtable(filter: { table: { eq: "events" }} ) {
+    edges {
+      node {
+        data {
+          eventName
+          eventGroup
+        }
+      } 
+    }
+  }
+}
+`
