f1589701f3923223389c067cbcff629a2b29f31b
Robert <soulliberty@gmail.com>
2019-02-12 18:23:43 -0600

    added event page stubs

--- src/pages/periods.js
+++ src/pages/periods.js
@@ -12,6 +12,13 @@ class PeriodsPage extends React.Component {
       <Layout>
         <div className="container">
           <h1>Timeline of the Bible</h1>
+          <div><i>The data for this page is under development.</i></div>
+          <div>&nbsp;</div>
+          {data.neo4j.EventGroup.map(group => (
+            <div>
+              {group.events[0].yearsOccurred[0].formattedYear} <Link to={'/period/'+group.slug}>{group.title}</Link>
+            </div>
+          ))}
         </div>
         <div className="footer"></div>
       </Layout>)
@@ -24,12 +31,15 @@ export const query = graphql
 `
 {
   neo4j {
-    EventGroup{
+    EventGroup(orderBy:sortKey_asc){
       title
       slug
-      events{
+      sortKey
+      events(orderBy:sequence_asc){
+        sequence
         yearsOccurred{
           year
+          formattedYear
         }
       }
     }
--- src/templates/periodTemplate.js
+++ src/templates/periodTemplate.js
@@ -4,53 +4,34 @@ import { Helmet } from 'react-helmet'
 import '../components/layout.css'
 import VerseList from '../components/VerseList'
 
-function Event(props) {
-  const event = props.eventData.node.data;
-  const people = (event.participants == null) ? false : 
-    (event.participants.map((person, i) =>
-      <>
-        {i > 0 && ', '}
-        <Link key={i} to={`/person/${person.data.slug}`}>{person.data.displayTitle}</Link>
-      </>
-    )
-  );
-  const places = (event.placeOccurred == null) ? false :
-    (event.placeOccurred.map((place, i) =>
-    <>
-      {i > 0 && ', '}
-      <Link key={i} to={`/place/${place.data.slug}`}>{place.data.displayTitle}</Link>
-    </>
-    )
-  );
+// function Event(props) {
+//   const event = props.eventData.node.data;
+//   const people = (event.participants == null) ? false : 
+//     (event.participants.map((person, i) =>
+//       <>
+//         {i > 0 && ', '}
+//         <Link key={i} to={`/person/${person.data.slug}`}>{person.data.displayTitle}</Link>
+//       </>
+//     )
+//   );
+//   const places = (event.placeOccurred == null) ? false :
+//     (event.placeOccurred.map((place, i) =>
+//     <>
+//       {i > 0 && ', '}
+//       <Link key={i} to={`/place/${place.data.slug}`}>{place.data.displayTitle}</Link>
+//     </>
+//     )
+//   );
 
-  return (
-    <>
-      <div><b>{event.eventName}</b></div>
-      {people && (<div>People: {people}</div>)}
-      {places && (<div>Places: {places}</div>)}
-      <div>Passages: <VerseList verses={event.versesDescribed} /></div>
-    </>
-  )
-}
-
-function Events(props) {
-  const events = props.eventData;
-  const eventList  = events.edges.map((event, i) => <Event key={i} eventData={event} />);
-
-  return (
-    <>
-      <div>
-        <h3>{events.edges[0].node.data.yearGroup}</h3>
-        <div>{eventList}</div>
-      </div>
-    </>
-  )
-}
-
-function Years(props) {
-  const years = props.periodData.group;
-  return Object.keys(years).map((year, i) => <Events key={i} eventData={years[year]} />)
-}
+//   return (
+//     <>
+//       <div><b>{event.eventName}</b></div>
+//       {people && (<div>People: {people}</div>)}
+//       {places && (<div>Places: {places}</div>)}
+//       <div>Passages: <VerseList verses={event.versesDescribed} /></div>
+//     </>
+//   )
+// }
 
 class Period extends React.Component {
 
@@ -60,13 +41,19 @@ class Period extends React.Component {
       <>
         <Helmet>
           <meta charSet="utf-8" />
-          <title>{data.allAirtable.group[0].edges[0].node.data.eventGroup}</title>
-          <meta content="{data.allAirtable.group[0].edges[0].node.data.eventGroup}" property="og:title" />
+          <title>{data.neo4j.EventGroup[0].title}</title>
+          <meta content="{data.neo4j.EventGroup[0].title}" property="og:title" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
         </Helmet>
         <div className="container">
-          <h1 className="heading">{data.allAirtable.group[0].edges[0].node.data.eventGroup}</h1>
-          <Years periodData={data.allAirtable}></Years>
+          <h1 className="heading">{data.neo4j.EventGroup[0].title}</h1>
+          {data.neo4j.EventGroup[0].years.map(year => (
+            <>
+            <div className="year-label">{year.formattedYear}</div>
+            <div></div>
+            </>
+          ))}
+          
           <div className="footer" />
         </div>
       </>
@@ -76,7 +63,33 @@ class Period extends React.Component {
 
 export default Period
 
-// export const pageQuery = graphql
-// `
-
-// `
+export const pageQuery = graphql `
+query ($lookupName: String!){
+  neo4j {
+    EventGroup(filter:{slug: $lookupName}) {
+      title
+      years(orderBy: year_asc) {
+        formattedYear
+        year
+        events(orderBy: sequence_asc) {
+          title
+          sequence
+          placeOccurred(orderBy: name_asc) {
+            name
+            slug
+          }
+          participants(orderBy: name_asc) {
+            name
+            slug
+          }
+          verses(orderBy: verseId_asc) {
+            verseId
+            osisRef
+            title
+          }
+        }
+      }
+    }
+  }
+}
+`
