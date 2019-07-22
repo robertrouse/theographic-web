1b451021cff8d436f97ccb0a6f68066f64295fbc
Robert <soulliberty@gmail.com>
2019-07-22 18:22:04 -0500

    emoji test

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -27,7 +27,7 @@ const EventList = (props) => {
                   <div>
                     <div>{event.title}</div>
                     {event.participants && event.participants.length > 0 &&
-                      <div>People: {event.participants.map((person, i) =>
+                      <div>👥 People: {event.participants.map((person, i) =>
                         <>
                           {i > 0 && ', '}
                           <Link key={i} to={'/person/' + person.slug}>{person.name}</Link>
@@ -35,7 +35,7 @@ const EventList = (props) => {
                       )}
                       </div>}
                     {event.placeOccurred && event.placeOccurred.length > 0 &&
-                      <div>Place(s): {event.placeOccurred.map((place, i) =>
+                      <div>📍 Place(s): {event.placeOccurred.map((place, i) =>
                         <>
                           {i > 0 && ', '}
                           <Link key={i} to={'/place/' + place.slug}>{place.name}</Link>
@@ -43,7 +43,7 @@ const EventList = (props) => {
                       )}
                       </div>}
                     {event.verses.length > 0 &&
-                      <div>Passages:
+                      <div>📖 Passages:
                       <Link to={event.verses[0].osisRef.split('.')[0].toLowerCase() + '#' + event.verses[0].osisRef}>
                           {event.verses[0].title}-
                           {(event.verses[0].osisRef.split('.')[1] !== event.verses[event.verses.length - 1].osisRef.split('.')[1]) &&
