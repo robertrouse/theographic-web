a7ceca771944b53b6cbc1ccc46524117a9eb2124
“robertrouse” <soulliberty@gmail.com>
2020-08-15 16:03:03 -0500

    bug fixes

--- src/components/search/SearchPane.js
+++ src/components/search/SearchPane.js
@@ -119,7 +119,7 @@ export default function SearchPane() {
         "versesLimit": 10,
         "versesOffset": 0
       },
-    skip: searchInput.length < 3,
+    skip: !searchInput || searchInput.length < 3,
     notifyOnNetworkStatusChange: true,
     fetchPolicy: "cache-and-network"
   });
